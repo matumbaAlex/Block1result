050e163ff44f816ae25310d1e52c5fb880d4013e
Alexey <super.balerun@yandex.ru>
2023-05-13 12:21:27 +0200

    add method Main

--- Program.cs
+++ Program.cs
@@ -2,4 +2,13 @@
 // длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 //  При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-  
+void Main()
+{
+    Console.Clear();
+    string mass = Input("Введите ваш массив строк через пробел или запятую");
+    string[] array = ArrayBuild(mass);
+    PrintArray(array);
+    int resultArrayLength = GetLength(array);
+    string[] result = GetResult(array, resultArrayLength);
+    PrintArray(result);
+}
